d5b0a58dd49ac01e223345659c2e023fd8657847
Vamsi Vaddavalli <ivamsi.dev@gmail.com>
2026-08-23 16:22:52 -0500

    Fix Dependabot failures on the Kotlin/JS yarn lockfile.
    Pin patched mocha transitives and ignore npm updates in kotlin-js-store, which has no real package.json.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.vanniktech.maven.publish) apply false
 }
+
+plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+    the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
+        resolution("diff", "8.0.3")
+        resolution("serialize-javascript", "7.0.5")
+    }
+}
